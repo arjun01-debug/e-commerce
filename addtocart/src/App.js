3bde267dcb7db9842b661083740cfeda1ceced15
arjun01-debug <arjunjangra01@gmail.com>
2024-12-02 01:47:22 +0530

    first commit

---
+++ addtocart/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
